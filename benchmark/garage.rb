7630a27382ebbb73f731afc050c5f19630a82de9
hokita <okita@enigmo.co.jp>
2020-10-05 13:33:28 +0900

    wip

--- benchmark/garage.rb
+++ benchmark/garage.rb
@@ -1,6 +1,23 @@
+# frozen_string_literal: true
+
 require 'garage'
+require 'ostruct'
 
+class BookResource
+  include Garage::Representer
 
+  property :id
+  property :title
+  property :description
+  property :published_at
+  # property :genre
+  # collection :authors
+  delegate :id, :title, :description, :published_at, :genre, to: :@model
+
+  def initialize(model)
+    @model = model
+  end
+end
 
 def run!
   # Fixture
@@ -8,8 +25,14 @@ def run!
     include Garage::HypermediaResponder
   end
 
-  controller =
+  controller = OpenStruct.new({
+                                :field_selector => Garage::NestedFieldQuery::Selector.build('*'),
+                                :params => { fields: 'fields' },
+                                :formats => [:json]
+                              })
 
-  responder = responder_class.new(controller, resource)
+  resource = BookResource.new(DATA.sample)
+  responder = responder_class.new(controller, [resource])
+
   responder.transform(resource)
 end
